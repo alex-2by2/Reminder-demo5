c7bad564d58330f9fa860f7bfd486ded832b7038
alex-2by2 <137150612+alex-2by2@users.noreply.github.com>
2026-07-25 16:24:27 +0530

    Delete js/translate-init.js

--- js/translate-init.js
+++
@@ -1,3 +0,0 @@
-function googleTranslateElementInit() { 
-    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,gu,hi', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element'); 
-}
